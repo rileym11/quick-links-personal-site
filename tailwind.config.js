f0ee6ae9fae5bc336579e0e551e4f7955bafe5eb
Riley MacDonald <32280362+rileym11@users.noreply.github.com>
2020-12-02 07:54:01 -0500

    cleanup: purge css in builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
