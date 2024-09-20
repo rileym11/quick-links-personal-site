4cf99eef8ff345e4b5c5cf5627321c397e0ebe50
Riley MacDonald <macdonald.riley11@gmail.com>
2024-09-20 10:35:40 -0300

    chore: update all deps and switch to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './src/**/*.js',
-  ],
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
-      sans: ['Orbitron', 'sans-serif'],
-      serif: ['serif'],
+      sans: ["Orbitron", "sans-serif"],
+      serif: ["serif"],
     },
   },
   variants: {
