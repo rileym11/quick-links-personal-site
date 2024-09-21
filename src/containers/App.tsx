861e793e1236890c94b3a78f4c8d94f855bd5c9d
Riley MacDonald <macdonald.riley11@gmail.com>
2024-09-21 15:12:41 -0300

    chore: replace location link with github link

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Mail, MapPin, FileText, Linkedin } from 'react-feather';
-import { withLayout } from '../components/Layout';
-import { NavItem } from '../components/NavItem';
+import React from "react";
+import { Mail, MapPin, FileText, Linkedin, GitHub } from "react-feather";
+import { withLayout } from "../components/Layout";
+import { NavItem } from "../components/NavItem";
 
 export function App() {
   return (
@@ -18,9 +18,9 @@ export function App() {
         <ul className="flex flex-col">
           <li className="flex"></li>
           <NavItem
-            icon={<MapPin />}
-            text="Montreal, QC"
-            link="https://www.google.ca/maps/place/Montreal,+QC/@45.5576996,-74.0104841,10z/data=!3m1!4b1!4m5!3m4!1s0x4cc91a541c64b70d:0x654e3138211fefef!8m2!3d45.5016889!4d-73.567256"
+            icon={<GitHub />}
+            text="Github"
+            link="https://github.com/rileym11"
           />
           <NavItem
             icon={<FileText />}
